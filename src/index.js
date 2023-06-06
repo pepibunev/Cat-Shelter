121f424228b6a51df0b4199bafdb9b59c34452c9
pepibunev <peterbunev9@gmail.com>
2023-06-07 02:10:02 +0300

    added mongoose and Cat model

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
+const dbConnect = require('./config/dbConfig');
 const routes = require('./routes');
 
 const app = express();
@@ -10,9 +11,11 @@ const PORT = 5000;
 expressConfig(app);
 handlebarsConfig(app);
 
+dbConnect()
+    .then(() => console.log('DB successfully connected'))
+    .catch(err => console.log('DB error: ', err.message));
+
 app.use(routes);
-
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`);
